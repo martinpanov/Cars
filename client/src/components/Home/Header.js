98d3b3e44ced259a3914ebeab790152295ccccb3
martinpanov <martin.panov98@gmail.com>
2023-06-14 14:10:40 +0300

    header section responsive and updated hero section

--- client/src/components/Home/Header.js
+++ client/src/components/Home/Header.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 export default function Header() {
     return (
         <>
-            <div className={styles["header-content"]}>
-                <div className={styles["header-content-text"]}>
-                    <h1>Buy Or Rent Your <br /> <span>Dream Car</span> Today!</h1>
-                    <p>Looking to buy or rent a car? Check the available cars<br /> for sale and rent and find the car  you've always wanted</p>
+            <div className={styles["hero-wrapper"]}>
+                <div className={styles["hero-content"]}>
+                    <h1 className={styles["title"]}>Buy Or Rent Your <span>Dream Car</span> Today!</h1>
+                    <h2 className={styles["subtitle"]}>Looking to buy or rent a car? Check the available cars<br /> for sale and rent and find the car  you've always wanted</h2>
                     <div className={styles["header-content-buttons"]}>
                         <Link className={styles["header-content-buy-car"]} to="/catalog">Buy Car</Link>
                         <Link className={styles["header-content-rent-car"]} to="/rentcar">Rent Car</Link>
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -7,7 +7,7 @@ import Header from './Header';
 export default function Home() {
     return (
         <>
-            <section id={styles.home}>
+            <section id={styles.hero}>
                 <Header />
             </section>
 
