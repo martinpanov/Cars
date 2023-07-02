899e67a375d9c6e052f6c3f5a60771b8e3dd1ff3
martinpanov <martin.panov98@gmail.com>
2023-07-02 23:29:53 +0300

    Not Found responsive and removed unused components

--- client/src/components/NotFound/NotFound.js
+++ client/src/components/NotFound/NotFound.js
@@ -3,11 +3,8 @@ import styles from './NotFound.module.css';
 export default function NotFound() {
     return (
         <section id={styles["chetiri-page"]}>
-            <div>
-                <h1>404</h1>
-                <p>Resource Not Found</p>
-            </div>
-
+            <h1>404</h1>
+            <h2>Resource Not Found</h2>
         </section>
     );
 }
